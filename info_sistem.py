209b1f4c8cb3cb9d00fb246e3ffee9f471a3a7af
Hadrianus Lima <hadrianus.lima.130@ufrn.edu.br>
2026-06-26 17:44:39 -0300

    Módulo de informações do sistema

---
+++ info_sistem.py
@@ -0,0 +1,16 @@
+def sobre_sistm():
+    from validacoes import limpar_terminal
+    rosa_inicio = "\033[1;31;45m"
+    rosa_final = "\033[m"
+    limpar_terminal()
+    print(rosa_inicio+"-------------------------------------------"+rosa_final)
+    print(rosa_inicio+"|               SOBRE O SISTEMA           |"+rosa_final)
+    print(rosa_inicio+"-------------------------------------------"+rosa_final)
+    print("|| PROGRAMA PARA GESTÃO DE LOJA FEMININA ||")
+    print("||           DESENVOLVIDO POR:           ||")
+    print("|| 1- hadrianus.lima.130@ufrn.edu.br     ||")
+    print("||              LICENÇA MIT              ||")
+    print("||  https://opensource.org/licenses/MIT  ||")
+    print(" \                                        /")
+    print("  ---------------------------------------")
+    input("APERTE ENTER PARA PROSSEGUIR")
