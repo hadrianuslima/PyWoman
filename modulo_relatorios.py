dd63ce3bdc9d35cc50bc0cdb5a7e0b25eb92bd5e
Hadrianus Lima <hadrianus.lima.130@ufrn.edu.br>
2026-07-03 20:49:13 -0300

    Adicionado a opção de produtos mais vendidos nos relatórios

--- modulo_relatorios.py
+++ modulo_relatorios.py
@@ -31,6 +31,9 @@ def relatorios():
             break
         elif resp7 == "1":
             limpar_terminal()
+            print(rosa_inicio+"------------------------------------"+rosa_final)
+            print(rosa_inicio+"|         LISTA DE PRODUTOS        |"+rosa_final)
+            print(rosa_inicio+"------------------------------------"+rosa_final)
             for categoria in produtos:
                 if categoria == "roupas":
                     for codigo in produtos[categoria]:
@@ -61,6 +64,9 @@ def relatorios():
             input("APERTE ENTER PARA PROSSEGUIR")     
         elif resp7 == "2":
             limpar_terminal()
+            print(rosa_inicio+"------------------------------------"+rosa_final)
+            print(rosa_inicio+"|          LISTA DE VENDAS         |"+rosa_final)
+            print(rosa_inicio+"------------------------------------"+rosa_final)
             for id_venda in vendas:
                 if vendas[id_venda][5] ==  True:
                     print("\033[1;32m----------------------------------\033[m")
@@ -76,6 +82,9 @@ def relatorios():
             input("APERTE ENTER PARA PROSSEGUIR")
         elif resp7 == "3":
             limpar_terminal()
+            print(rosa_inicio+"------------------------------------"+rosa_final)
+            print(rosa_inicio+"|         LISTA DE CLIENTES        |"+rosa_final)
+            print(rosa_inicio+"------------------------------------"+rosa_final)
             for cpf_cliente in clientes:
                 if clientes[cpf_cliente][4] == True:
                     print("\033[1;32m----------------------------------\033[m")
@@ -83,6 +92,42 @@ def relatorios():
                     print(f"NOME: {clientes[cpf_cliente][0]}")
                     print(f"TELEFONE: {clientes[cpf_cliente][1]}")
                     print(f"DATA DE NASCIMENTO: {clientes[cpf_cliente][3]}")
+                    print("\033[1;32m----------------------------------\033[m")
+            input("APERTE ENTER PARA PROSSEGUIR")
+        elif resp7 == "4": 
+            limpar_terminal()
+            mais_vend = {}
+            for categoria in produtos:
+                for codigo in produtos[categoria]:
+                    qnt_codigo = 0
+                    for id_venda in vendas:
+                        for cod_produt in vendas[id_venda][3]:
+                            if cod_produt == codigo:
+                                qnt_codigo += vendas[id_venda][3][cod_produt]
+                    if qnt_codigo > 0:            
+                        mais_vend[codigo] = qnt_codigo
+            mais_vend = dict(sorted(mais_vend.items(),key=lambda item: item[1], reverse=True))
+            print(rosa_inicio+"------------------------------------"+rosa_final)
+            print(rosa_inicio+"|      PRODUTOS MAIS VENDIDOS      |"+rosa_final)
+            print(rosa_inicio+"------------------------------------"+rosa_final)
+            for produt in mais_vend:
+                if produt in produtos["roupas"]:
+                    print("\033[1;32m----------------------------------\033[m")
+                    print(f"Produto: {produtos["roupas"][produt][0]}")
+                    print(f"Codigo: {produt}")
+                    print(f"Quantidade: {mais_vend[produt]}")
+                    print("\033[1;32m----------------------------------\033[m")
+                elif produt in produtos["cosmeticos"]:
+                    print("\033[1;32m----------------------------------\033[m")
+                    print(f"Produto: {produtos["cosmeticos"][produt][0]}")
+                    print(f"Codigo: {produt}")
+                    print(f"Quantidade: {mais_vend[produt]}")
+                    print("\033[1;32m----------------------------------\033[m")
+                elif produt in produtos["acessorios"]:
+                    print("\033[1;32m----------------------------------\033[m")
+                    print(f"Produto: {produtos["acessorios"][produt][0]}")
+                    print(f"Codigo: {produt}")
+                    print(f"Quantidade: {mais_vend[produt]}")
                     print("\033[1;32m----------------------------------\033[m")
             input("APERTE ENTER PARA PROSSEGUIR")
         else:
