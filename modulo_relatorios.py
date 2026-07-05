12eb3835dec22313863bbe2175f551dc865e7a64
Hadrianus Lima <hadrianus.lima.130@ufrn.edu.br>
2026-07-04 23:26:16 -0300

    Adiconado a função de mostrar as preferências de produtos de cada cliente

--- modulo_relatorios.py
+++ modulo_relatorios.py
@@ -1,7 +1,7 @@
 def relatorios():
     import pickle
     from recup_dados import recuperar_clientes,recuperar_produtos,recuperar_vendas
-    from validacoes import limpar_terminal
+    from validacoes import limpar_terminal, verificar_letras, validar_cpf
 
     rosa_inicio = "\033[1;31;45m"
     rosa_final = "\033[m"
@@ -96,22 +96,23 @@ def relatorios():
             input("APERTE ENTER PARA PROSSEGUIR")
         elif resp7 == "4": 
             limpar_terminal()
-            mais_vend = {}
-            for categoria in produtos:
-                for codigo in produtos[categoria]:
-                    qnt_codigo = 0
-                    for id_venda in vendas:
-                        for cod_produt in vendas[id_venda][3]:
-                            if cod_produt == codigo:
-                                qnt_codigo += vendas[id_venda][3][cod_produt]
-                    if qnt_codigo > 0:            
-                        mais_vend[codigo] = qnt_codigo
-            mais_vend = dict(sorted(mais_vend.items(),key=lambda item: item[1], reverse=True))
             print(rosa_inicio+"------------------------------------"+rosa_final)
             print(rosa_inicio+"|      PRODUTOS MAIS VENDIDOS      |"+rosa_final)
             print(rosa_inicio+"------------------------------------"+rosa_final)
+            mais_vend = {}
+            for id_venda in vendas:
+                if vendas[id_venda][5] == True:
+                    for cod_produt in vendas[id_venda][3]:
+                        if cod_produt not in mais_vend:
+                            mais_vend[cod_produt] = vendas[id_venda][3][cod_produt]
+                        else:
+                            mais_vend[cod_produt] += vendas[id_venda][3][cod_produt]
+            mais_vend = dict(sorted(mais_vend.items(),key=lambda item: item[1], reverse=True))
+            contador = 0 
             for produt in mais_vend:
-                if produt in produtos["roupas"]:
+                if contador == 5: #Mostrar somente os 5 produtos mais vendidos
+                    break
+                elif produt in produtos["roupas"]:
                     print("\033[1;32m----------------------------------\033[m")
                     print(f"Produto: {produtos["roupas"][produt][0]}")
                     print(f"Codigo: {produt}")
@@ -129,11 +130,68 @@ def relatorios():
                     print(f"Codigo: {produt}")
                     print(f"Quantidade: {mais_vend[produt]}")
                     print("\033[1;32m----------------------------------\033[m")
+                contador += 1
             input("APERTE ENTER PARA PROSSEGUIR")
         else:
-            print()
-            print(rosa_inicio+"-----------------------------------"+rosa_final)
-            print(rosa_inicio+"|    FUNÇÃO EM DESENVOLVIMENTO⚠️   |"+rosa_final)
-            print(rosa_inicio+"-----------------------------------"+rosa_final)
-            print()
+            limpar_terminal()
+            print(rosa_inicio+"------------------------------------"+rosa_final)
+            print(rosa_inicio+"|      PREFERÊNCIA DE CLIENTES     |"+rosa_final)
+            print(rosa_inicio+"------------------------------------"+rosa_final)
+            nome_client = input("Informe o nome do cliente que deseja ver as preferências: ").strip().lower()
+            while verificar_letras(nome_client) == False:
+                nome_client = input("SOMENTE LETRAS: ").strip().lower()
+            achou = ""
+            cpf_mostrados = []
+            for id_venda in vendas:
+                if nome_client in vendas[id_venda][0].lower() and vendas[id_venda][5] == True and vendas[id_venda][1] not in cpf_mostrados:
+                    achou = "S"
+                    cpf_mostrados += [vendas[id_venda][1]]
+                    print("\033[1;32m----------------------------------\033[m")
+                    print(f"CPF: {vendas[id_venda][1]}")
+                    print(f"NOME: {vendas[id_venda][0]}")
+                    print("\033[1;32m----------------------------------\033[m")
+            if achou != "S":
+                print("Esse cliente ainda não realizou compra.")
+            else:
+                cpf_cliente = input("Informe o CPF do cliente desejado: ").strip()
+                while validar_cpf(cpf_cliente) == False:
+                    cpf_cliente = input("Informe o CPF de forma correta: ").strip()
+                prefe_client = {}
+                for id_venda in vendas:
+                    if cpf_cliente == vendas[id_venda][1] and vendas[id_venda][5] == True:
+                        for cod_produt in vendas[id_venda][3]:
+                            if cod_produt not in prefe_client:
+                                prefe_client[cod_produt] = vendas[id_venda][3][cod_produt]
+                            else:
+                                prefe_client[cod_produt] += vendas[id_venda][3][cod_produt]
+                prefe_client = dict(sorted(prefe_client.items(),key=lambda item: item[1], reverse=True))
+                #print(prefe_client)
+                limpar_terminal()
+                print(rosa_inicio+"------------------------------------"+rosa_final)
+                print(rosa_inicio+"|      PREFERÊNCIA DE CLIENTES     |"+rosa_final)
+                print(rosa_inicio+"------------------------------------"+rosa_final)
+                print(f"CLIENTE: {clientes[cpf_cliente][0]}")
+                contador = 0 
+                for produt in prefe_client:
+                    if contador == 5: #Mostrar somente os 5 produtos mais vendidos
+                        break
+                    elif produt in produtos["roupas"]:
+                        print("\033[1;32m----------------------------------\033[m")
+                        print(f"Produto: {produtos["roupas"][produt][0]}")
+                        print(f"Codigo: {produt}")
+                        print(f"Quantidade: {prefe_client[produt]}")
+                        print("\033[1;32m----------------------------------\033[m")
+                    elif produt in produtos["cosmeticos"]:
+                        print("\033[1;32m----------------------------------\033[m")
+                        print(f"Produto: {produtos["cosmeticos"][produt][0]}")
+                        print(f"Codigo: {produt}")
+                        print(f"Quantidade: {prefe_client[produt]}")
+                        print("\033[1;32m----------------------------------\033[m")
+                    elif produt in produtos["acessorios"]:
+                        print("\033[1;32m----------------------------------\033[m")
+                        print(f"Produto: {produtos["acessorios"][produt][0]}")
+                        print(f"Codigo: {produt}")
+                        print(f"Quantidade: {prefe_client[produt]}")
+                        print("\033[1;32m----------------------------------\033[m")
+                    contador += 1
             input("APERTE ENTER PARA PROSSEGUIR")
